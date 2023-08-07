1c356a857648d89f81864f71ee5386c728c39edd
Lucian Bellini <lucianbellini1@gmail.com>
2023-08-07 20:00:24 -0300

    Adding FileWriter

--- src/br/com/adressFinder/main/Main.java
+++ src/br/com/adressFinder/main/Main.java
@@ -3,15 +3,21 @@ package br.com.adressFinder.main;
 import br.com.adressFinder.model.Adress;
 import br.com.adressFinder.api.cepApi;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner leitura = new Scanner(System.in);
         String entrada = " ";
         String nome = " ";
+        List<Adress> adresses = new ArrayList<>();
 
         while (!entrada.equalsIgnoreCase("sair")) {
             System.out.println("***********************************");
@@ -24,9 +30,15 @@ public class Main {
 
             Adress cep = new Adress();
             cep.setCEP(entrada);
+            adresses.add(cep);
 
             cepApi cepAPI = new cepApi();
             cepAPI.searchCep(cep);
+
         }
+
+        FileWriter write = new FileWriter("Data.json");
+        write.write(adresses.toString());
+        write.close();
     }
 }
